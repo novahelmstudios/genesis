572cc9da5957f1a57d8083985c158b6ca6bb7ade
abdulahshoaib <khan.abdullah.shoaib@gmail.com>
2025-06-15 01:34:01 +0500

    feat: init implementation of game component classes

---
+++ src/game/game.cpp
@@ -0,0 +1,18 @@
+#include "game/game.hpp"
+#include "game/player.hpp"
+
+Game::Game() {
+    // initialize entities
+}
+
+Game::~Game() {}
+
+void Game::init() {}
+
+void Game::update(float deltaTime) {
+    // update entities
+}
+
+void Game::render() {
+    // render entities
+}
---
+++ src/game/player.cpp
@@ -0,0 +1,12 @@
+#include "game/player.hpp"
+#include <iostream>
+
+Player::Player() {}
+
+void Player::update(float deltaTime) {
+    // movement/input
+}
+
+void Player::render() {
+    // draw player
+}
