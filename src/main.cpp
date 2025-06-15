f04058f1c45d1b82bc649565496f99b756e3a29b
abdulahshoaib <khan.abdullah.shoaib@gmail.com>
2025-06-15 07:59:09 +0500

    feat: try-catch added

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include "core/application.hpp"
+#include <iostream>
 
 int main() {
-  Application app(1280, 720, "OpenGL Game");
-  app.run();
-  return 0;
+  try {
+    Application app(1280, 720, "OpenGL Game");
+    app.run();
+  } catch (const std::exception &e) {
+    std::cerr << "Fatal error: " << e.what() << '\n';
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
 }
