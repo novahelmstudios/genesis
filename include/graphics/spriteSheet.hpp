e5acfdaa194ecc55ccf5b7b2ec76b7dc8ed09b86
abdulahshoaib <khan.abdullah.shoaib@gmail.com>
2025-06-21 04:42:02 +0500

    feat: added default constructor for the sprite sheet struct

--- include/graphics/spriteSheet.hpp
+++ include/graphics/spriteSheet.hpp
@@ -18,8 +18,8 @@ struct SpriteSheet {
   int totalFrames;
   float frameDuration; /* in sec */
 
-  SpriteSheet(const string &tex, int fw = 0, int fh = 0, int col = 0,
-              int r = 0, int _totalFrames = 0, float dur = 0.0f)
+  SpriteSheet(const string &tex, int fw = 0, int fh = 0, int col = 0, int r = 0,
+              int _totalFrames = 0, float dur = 0.0f)
       : textureName(tex), frameWidth(fw), frameHeight(fh), column(col), rows(r),
         totalFrames(_totalFrames), frameDuration(dur) {}
 };
