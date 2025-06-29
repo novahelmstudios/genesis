32349f3f3edc87183b6b1d668feabcef794397a7
abdulahshoaib <khan.abdullah.shoaib@gmail.com>
2025-06-29 20:00:27 +0500

    feat: testing the window

---
+++ test/window.cpp
@@ -0,0 +1,23 @@
+#include "window_manager/window.h"
+
+int main() {
+    Engine::WindowConfig config;
+    config.fullscreen = false; // make it true for windowless fullscreen
+    config.maximized = true; // makes it windowed fullscreen
+    config.resizable = true; //
+    //config.x_postion = 100;  // optional
+    //config.y_postion = 80;
+    config.title = "Game App";
+
+    Engine::Window myWindow(config);
+    myWindow.setIcon("../../assets/icons/icon.png");
+    while (!myWindow.shouldClose()) {
+        glClearColor(0.1f, 0.1f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        myWindow.swapBuffers();
+        myWindow.pollEvents();
+    }
+
+    return 0;
+}
