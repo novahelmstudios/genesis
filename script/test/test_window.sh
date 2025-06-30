317338b1dba37665698cf8f2a0e97d98d14bb603
abdulahshoaib <khan.abdullah.shoaib@gmail.com>
2025-06-30 13:19:32 +0500

    feat: added scripts to run the game an testing the engine modules

--- script/test/test_window.sh
+++ script/test/test_window.sh
@@ -8,9 +8,9 @@ ENGINE_INC="-Iengine/inc -Iengine/inc/external -I/usr/include/freetype2 -I/usr/i
 ENGINE_SRC="engine/src/window_manager/window.cpp engine/src/glad.c"
 TEST_SRC="test/window.cpp"
 LIBS="-lglfw -lGL -ldl -lpthread -lm"
-OUT="build/window_test"
+OUT="build/test/window_test"
 
-mkdir -p build
+mkdir -p build/test
 
 g++ -std=c++23 $ENGINE_INC -Wall -Wextra $TEST_SRC $ENGINE_SRC $LIBS -o $OUT
 
